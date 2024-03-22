f9a9429a193cad87da907f8353568013ccc776ab
Edison Chung <edchung482@gmail.com>
2024-03-22 01:26:30 -0700

    added comment headers

--- model/market.js
+++ model/market.js
@@ -1,11 +1,18 @@
+/*
+ * File: market.js
+ * Author: Edison Chung
+ * Date: 3/22/24
+ * Description: Defines the mongoose model for the market collection.
+ */
+
 const mongoose = require("mongoose");
 
 const marketSchema = new mongoose.Schema(
     {
-        stockName: { type: String, required: true },
-        value: Number,
-        description: String,
-        lastUpdated: { type: Date, default: Date.now },
+        stockName: { type: String, required: true }, // name of the stock
+        value: Number,                               // price of the stock
+        description: String,                            // description of the stock
+        lastUpdated: { type: Date, default: Date.now }, // last updated date
     },
     { collections: "market" }
 );
